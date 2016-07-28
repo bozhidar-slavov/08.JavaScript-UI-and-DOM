bfd410a982898b3d1fdd5720451163a082f9de0f
bozhidar.slavov <bozhidar.slavov96@gmail.com>
2016-07-28 15:58:42 +0300

    Dom operations homework added

---
+++ 01.DOM-Operations/task-1.js
@@ -0,0 +1,44 @@
+﻿function solve() {
+    return function (element, contents) {
+        var elem,
+            firstChild,
+            fragment,
+            i,
+            len,
+            newDiv,
+            divToAdd;
+
+        if (typeof (element) !== 'string' && !(element instanceof HTMLElement)) {
+            throw '';
+        }
+
+        if (typeof (element) === 'string') {
+            elem = document.getElementById(element)
+        } else {
+            elem = element;
+        }
+
+        if (!contents || contents.some(function (item) {
+                return (typeof (item) !== 'string' && typeof (item) !== 'number');
+        })) {
+            throw '';
+        }
+
+        firstChild = elem.firstChild;
+        while (elem.firstChild) {
+            elem.removeChild(firstChild);
+            firstChild = firstChild.nextSibling;
+        }
+
+        newDiv = document.createElement('div');
+        fragment = document.createDocumentFragment();
+
+        for (i = 0, len = contents.length; i < len; i += 1) {
+            divToAdd = newDiv.cloneNode(true);
+            divToAdd.innerHTML = contents[i];
+            fragment.appendChild(divToAdd);
+        }
+
+        elem.appendChild(fragment);
+    };
+};
