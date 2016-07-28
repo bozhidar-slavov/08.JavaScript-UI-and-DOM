eb3bff1df0355068751163706e475ec04cd91e00
bozhidar.slavov <bozhidar.slavov96@gmail.com>
2016-07-28 22:57:56 +0300

    Fix

--- 02.Event-Model/task-1.js
+++ 02.Event-Model/task-1.js
@@ -1,18 +1,17 @@
 function solve(){
     return function (selector) {
-        if(typeof(selector) !== 'string' && !(selector instanceof HTMLElement)){
-            throw 'Neither a string nor an element';
-        }
-
-        if(document.getElementById(selector) === null){
+		if(document.getElementById(selector) === null){
             throw 'selects nothing';
+        }
+		
+        if(typeof(selector) !== 'string' && !(selector instanceof HTMLElement)){
+            throw 'Neither a string not an element';
         }
 
         var buttons = document.getElementsByClassName('button'),
             content = document.getElementsByClassName('content'),
             i, len;
-
-        for(i = 0, len = buttons.length; i < len; i+=1){
+        for(i = 0, len = buttons.length; i < len; i += 1){
             buttons[i].innerHTML = 'hide';
             buttons[i].addEventListener('click', function(ev){
                 var clickedButton = ev.target;
